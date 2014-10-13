74d07d661756e17b4120285e9085e73b33b1f88b
Omid Ghomeshi <omidghomeshi@gmail.com>
2014-10-13 17:33:22 -0400

    Added more parse objects

--- app/src/main/java/com/mhacks/android/data/model/Award.java
+++ app/src/main/java/com/mhacks/android/data/model/Award.java
@@ -1,5 +1,8 @@
 package com.mhacks.android.data.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
 
@@ -7,6 +10,70 @@ import com.parse.ParseObject;
  * Created by Omid Ghomeshi on 10/13/14.
  */
 @ParseClassName("Award")
-public class Award extends ParseObject {
+public class Award extends ParseObject implements Parcelable {
 
+    public static final String DESCRIPTION_COL = "description";
+    public static final String PRIZE_COL       = "prize";
+    public static final String SPONSOR_COL     = "sponsor";
+    public static final String TITLE_COL       = "title";
+    public static final String VALUE_COL       = "value";
+    public static final String WEBSITE_COL     = "website";
+
+    public String getDescription() {
+        return getString(DESCRIPTION_COL);
+    }
+
+    public void setDescription(String description) {
+        put(DESCRIPTION_COL, description);
+    }
+
+    public String getPrize() {
+        return getString(PRIZE_COL);
+    }
+
+    public void setPrize(String prize) {
+        put(PRIZE_COL, prize);
+    }
+
+    public Sponsor getSponsor() {
+        return (Sponsor) getParseObject(SPONSOR_COL);
+    }
+
+    public void setSponsor(Sponsor sponsor) {
+        put(SPONSOR_COL, sponsor);
+    }
+
+    public String getTitle() {
+        return getString(TITLE_COL);
+    }
+
+    public void setTitle(String title) {
+        put(TITLE_COL, title);
+    }
+
+    public int getValue() {
+        return getInt(VALUE_COL);
+    }
+
+    public void setValue(int value) {
+        put(VALUE_COL, value);
+    }
+
+    public String getWebsite() {
+        return getString(WEBSITE_COL);
+    }
+
+    public void setWebsite(String website) {
+        put(WEBSITE_COL, website);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
 }
--- app/src/main/java/com/mhacks/android/data/model/Event.java
+++ app/src/main/java/com/mhacks/android/data/model/Event.java
@@ -3,10 +3,78 @@ package com.mhacks.android.data.model;
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
 
+import org.json.JSONArray;
+
+import java.util.Date;
+
 /**
  * Created by Omid Ghomeshi on 10/13/14.
  */
 @ParseClassName("Event")
 public class Event extends ParseObject {
+
+    public static final String CATEGORY_COL   = "category";
+    public static final String DETAILS_COL    = "details";
+    public static final String DURATION_COL   = "duration";
+    public static final String HOST_COL       = "host";
+    public static final String LOCATIONS_COL  = "locations";
+    public static final String START_TIME_COL = "startTime";
+    public static final String TITLE_COL      = "title";
+
+    public EventType getCategory() {
+        return (EventType) getParseObject(CATEGORY_COL);
+    }
+
+    public void setCategory(EventType category) {
+        put(CATEGORY_COL, category);
+    }
+
+    public String getDetails() {
+        return getString(DETAILS_COL);
+    }
+
+    public void setDetails(String details) {
+        put(DETAILS_COL, details);
+    }
+
+    public int getDuration() {
+        return getInt(DURATION_COL);
+    }
+
+    public void setDuration(int duration) {
+        put(DURATION_COL, duration);
+    }
+
+    public Sponsor getHost() {
+        return (Sponsor) getParseObject(HOST_COL);
+    }
+
+    public void setHost(Sponsor sponsor) {
+        put(HOST_COL, sponsor);
+    }
+
+    public JSONArray getLocations() {
+        return getJSONArray(LOCATIONS_COL);
+    }
+
+    public void setLocations(JSONArray locations) {
+        put(LOCATIONS_COL, locations);
+    }
+
+    public Date getStartTime() {
+        return getDate(START_TIME_COL);
+    }
+
+    public void setStartTime(Date date) {
+        put(START_TIME_COL, date);
+    }
+
+    public String getTitle() {
+        return getString(TITLE_COL);
+    }
+
+    public void setTitle(String title) {
+        put(TITLE_COL, title);
+    }
 
 }
