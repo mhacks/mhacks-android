926bf1f886c6946c11af10e9700c5c768e8af677
Omkar Moghe <theomkarmoghe@gmail.com>
2014-10-23 15:55:47 -0400

    Structural changes to the NavigationDrawer
    Changed from listview to textview inside drawer frag. Using callbacks to
    handle view changes.

--- app/src/main/java/com/mhacks/android/ui/MainActivity.java
+++ app/src/main/java/com/mhacks/android/ui/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.widget.DrawerLayout;
 import android.widget.ListView;
 
 import com.mhacks.android.ui.nav.NavigationDrawerFragment;
@@ -13,7 +12,9 @@ import com.mhacks.iv.android.R;
 
 import java.util.Date;
 
-
+/**
+ * Created by Omkar Moghe on 10/22/2014.
+ */
 public class MainActivity extends FragmentActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
@@ -27,15 +28,14 @@ public class MainActivity extends FragmentActivity
 
     private boolean mShouldSync = true;
 
-    private DrawerLayout drawerLayout;
-    private ListView drawerListView;
+    private ListView featuresListView;
+
+    private String[] featuresList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-        drawerListView = (ListView)findViewById(R.id.drawer_list_view);
 
         mNavigationDrawerFragment = new NavigationDrawerFragment();
         FragmentManager fragmentManager = getFragmentManager();
@@ -63,11 +63,13 @@ public class MainActivity extends FragmentActivity
     }
 */
 
-    public void restoreActionBar() {
+    public void restoreActionBar(String title) {
         final ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
+        if (actionBar != null) {
+            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+            actionBar.setDisplayShowTitleEnabled(true);
+            actionBar.setTitle(title);
+        }
     }
 
     @Override
--- app/src/main/java/com/mhacks/android/ui/nav/NavigationDrawerFragment.java
+++ app/src/main/java/com/mhacks/android/ui/nav/NavigationDrawerFragment.java
@@ -6,20 +6,39 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.mhacks.iv.android.R;
 
 /**
  * Created by Omkar Moghe on 10/22/2014.
  */
-public class NavigationDrawerFragment extends Fragment{
+public class NavigationDrawerFragment extends Fragment implements View.OnClickListener {
+    private View mNavDrawerView;
+
+    private TextView announcementsTextView, scheduleTextView, sponsorsTextView, awardsTextView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        mNavDrawerView = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+
+        //TODO instantiate all text view
+        //TODO set onclicklisteners for all textviews
+
+        return mNavDrawerView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        //TODO text click logic to change views, action bar, etc.
     }
 
     public interface NavigationDrawerCallbacks {
