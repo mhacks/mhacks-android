50ec0d72af0461914be683d2febd72a1f79374bb
ashsax <ashwin.222@gmail.com>
2014-11-11 17:08:23 -0500

    first commit. setup GridView and started adapter

--- app/src/main/java/com/mhacks/android/ui/nav/AwardsFragment.java
+++ app/src/main/java/com/mhacks/android/ui/nav/AwardsFragment.java
@@ -1,11 +1,13 @@
 package com.mhacks.android.ui.nav;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 import com.mhacks.iv.android.R;
 
@@ -26,5 +28,40 @@ public class AwardsFragment extends Fragment{
         //Put code for instantiating views, etc here. (before the return statement.)
 
         return mAwardsFragView;
+    }
+
+    private class CustomGrid extends BaseAdapter {
+
+        private Context mContext;
+
+        public CustomGrid(Context c) {
+            mContext = c;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View grid;
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            if (convertView == null) {
+                grid = new View(mContext);
+            }
+            grid = inflater.inflate(R.layout.award_grid_item, null);
+        }
+
     }
 }
