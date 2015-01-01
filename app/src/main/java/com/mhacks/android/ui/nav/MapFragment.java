f9782d75efcdd8375d926aea5efcc6643ac842d6
Omkar Moghe <omoghe@umich.edu>
2014-12-31 19:34:53 -0800

    Map query working

--- app/src/main/java/com/mhacks/android/ui/nav/MapFragment.java
+++ app/src/main/java/com/mhacks/android/ui/nav/MapFragment.java
@@ -3,11 +3,21 @@ package com.mhacks.android.ui.nav;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.mhacks.android.data.model.Map;
+import com.mhacks.android.ui.common.ImageLoader;
 import com.mhacks.iv.android.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Omkar Moghe on 12/31/2014.
@@ -16,10 +26,19 @@ import com.mhacks.iv.android.R;
  */
 public class MapFragment extends Fragment {
 
-    public static final String TAG = "ScheduleFragment";
+    public static final String TAG = "MapFragment";
 
     //Views
-    private View mMapFragView;
+    private View      mMapFragView;
+    private ImageView mapView;
+
+    //Map object list
+    private List<Map> maps;
+
+    //ImageLoader
+    ImageLoader imageLoader;
+
+    private String[] tabs = {"ALL", "BBB", "EECS", "GG Brown"};
 
     @Nullable
     @Override
@@ -28,6 +47,29 @@ public class MapFragment extends Fragment {
                              Bundle savedInstanceState) {
         mMapFragView = inflater.inflate(R.layout.fragment_map, container, false);
 
+        mapView = (ImageView) mMapFragView.findViewById(R.id.map_view);
+
+        getMaps();
+
         return mMapFragView;
+    }
+
+    /**
+     * Queries the Parse database for Map objects.
+     */
+    public void getMaps() {
+        ParseQuery<Map> query = ParseQuery.getQuery("Map");
+        query.findInBackground(new FindCallback<Map>() {
+            @Override
+            public void done(List<Map> mapsList, ParseException e) {
+                if (e == null) {
+                    maps = new ArrayList<Map>(mapsList);
+//                    imageLoader.DisplayImage(maps.get(0).getImage().getUrl(), mapView);
+                    Log.v(TAG, maps.size() + " maps");
+                } else {
+                    Log.e(TAG, "Query failed.", e);
+                }
+            }
+        });
     }
 }
