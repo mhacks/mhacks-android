98b57a39fc98827023ef1d668b703b173f7a7e54
Ben Oztalay <boztalay@gmail.com>
2015-01-15 16:32:45 -0500

    Changed Maps to use Picasso for image loading'

--- app/src/main/java/com/mhacks/android/ui/nav/MapFragment.java
+++ app/src/main/java/com/mhacks/android/ui/nav/MapFragment.java
@@ -1,12 +1,8 @@
 package com.mhacks.android.ui.nav;
 
 import android.app.Fragment;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,14 +14,12 @@ import android.widget.Spinner;
 
 import com.mhacks.android.data.model.Map;
 import com.mhacks.android.ui.MainActivity;
-import com.mhacks.android.ui.common.ImageLoader;
 import com.mhacks.iv.android.R;
 import com.parse.FindCallback;
-import com.parse.GetDataCallback;
 import com.parse.ParseException;
-import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,7 +27,7 @@ import java.util.List;
 /**
  * Created by Omkar Moghe on 12/31/2014.
  *
- * Displays maps of the MHacks V venues.
+ * Displays mMaps of the MHacks V venues.
  */
 public class MapFragment extends Fragment implements AdapterView.OnItemSelectedListener{
 
@@ -48,7 +42,7 @@ public class MapFragment extends Fragment implements AdapterView.OnItemSelectedL
     private Spinner mapSpinner;
 
     private ArrayList<String> mapNames;
-    private ArrayList<Map> maps;
+    private ArrayList<Map> mMaps;
 
     @Nullable
     @Override
@@ -59,7 +53,6 @@ public class MapFragment extends Fragment implements AdapterView.OnItemSelectedL
 
         //Instantiate lists.
         mapNames = new ArrayList<>();
-
 
         //Inflate spinner view.
         mapSpinner = (Spinner) getActivity().findViewById(R.id.map_spinner);
@@ -94,9 +87,9 @@ public class MapFragment extends Fragment implements AdapterView.OnItemSelectedL
             @Override
             public void done(List<Map> mapList, ParseException e) {
                 if (e != null || mapList == null || mapList.size() <= 0) {
-                    Log.e(TAG, "Couldn't get the local maps, falling back on remote");
+                    Log.e(TAG, "Couldn't get the local mMaps, falling back on remote");
                 } else {
-                    Log.d(TAG, "Got the local maps");
+                    Log.d(TAG, "Got the local mMaps");
                     processMapList(mapList);
                 }
 
@@ -112,10 +105,10 @@ public class MapFragment extends Fragment implements AdapterView.OnItemSelectedL
             mapNames.add("");
         }
 
-        //Instantiate maps as a copy of mapList
-        maps = new ArrayList<Map>(mapList);
+        //Instantiate mMaps as a copy of mapList
+        mMaps = new ArrayList<Map>(mapList);
         for (Map m : mapList) {
-            maps.set(m.getOrder(), m);
+            mMaps.set(m.getOrder(), m);
             mapNames.set(m.getOrder(), m.getTitle());
         }
 
@@ -131,23 +124,23 @@ public class MapFragment extends Fragment implements AdapterView.OnItemSelectedL
             @Override
             public void done(List<Map> mapList, ParseException e) {
                 if(e != null || mapList == null || mapList.size() <= 0) {
-                    Log.e(TAG, "Couldn't get the remote maps");
+                    Log.e(TAG, "Couldn't get the remote mMaps");
 
                     // We don't have anything locally, let the user know we need internet
-                    if(maps == null || maps.size() <= 0) {
+                    if(mMaps == null || mMaps.size() <= 0) {
                         if(getActivity() != null) ((MainActivity)getActivity()).showNoInternetOverlay();
                     } else {
                         // We do have local stuff, no make sure we aren't in the way
                         if(getActivity() != null) ((MainActivity)getActivity()).hideNoInternetOverlay();
                     }
                 } else {
-                    Log.d(TAG, "Got the remote maps, displaying them");
+                    Log.d(TAG, "Got the remote mMaps, displaying them");
 
-                    // We got the remote maps, so unpin the old ones and pin the new ones
+                    // We got the remote mMaps, so unpin the old ones and pin the new ones
                     ParseObject.unpinAllInBackground(MAP_PIN);
                     ParseObject.pinAllInBackground(MAP_PIN, mapList);
 
-                    // Display the new maps and get outta the way
+                    // Display the new mMaps and get outta the way
                     processMapList(mapList);
                     if(getActivity() != null) ((MainActivity)getActivity()).hideNoInternetOverlay();
                 }
@@ -156,7 +149,7 @@ public class MapFragment extends Fragment implements AdapterView.OnItemSelectedL
     }
 
     /**
-     * Sets the String array adapter to the names of the maps pulled from Parse. These are displayed
+     * Sets the String array adapter to the names of the mMaps pulled from Parse. These are displayed
      * in the spinner on the SupportActionBar (Toolbar).
      */
     public void setAdapter () {
@@ -169,12 +162,7 @@ public class MapFragment extends Fragment implements AdapterView.OnItemSelectedL
     }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        maps.get(position).getImage().getDataInBackground(new GetDataCallback() {
-            @Override
-            public void done(byte[] bytes, ParseException e) {
-                mapView.setImageBitmap(BitmapFactory.decodeByteArray(bytes, 0, bytes.length));
-            }
-        });
+        Picasso.with(getActivity()).load(mMaps.get(position).getImage().getUrl()).into(mapView);
     }
 
     @Override
