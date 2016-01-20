ef99b5f862d3d49936c87911e1d137ed3751d31c
Ankit Shah <ank.h.shah@gmail.com>
2016-01-19 22:23:59 -0500

    Adding the map

--- app/src/main/java/com/mhacks/android/ui/map/MapViewFragment.java
+++ app/src/main/java/com/mhacks/android/ui/map/MapViewFragment.java
@@ -1,6 +1,7 @@
 package com.mhacks.android.ui.map;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -11,7 +12,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 
-
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.GoogleMap;
 import com.mhacks.android.ui.MainActivity;
 import org.mhacks.android.R;
 import com.squareup.picasso.Picasso;
@@ -24,7 +27,7 @@ import java.util.List;
  *
  * Displays mMaps of the MHacks V venues.
  */
-public class MapViewFragment extends Fragment implements AdapterView.OnItemSelectedListener{
+public class MapViewFragment extends Fragment implements AdapterView.OnItemSelectedListener, OnMapReadyCallback{
 
     public static final String TAG = "MapViewFragment";
     public static final String MAP_PIN = "mapPin";
@@ -40,16 +43,36 @@ public class MapViewFragment extends Fragment implements AdapterView.OnItemSelec
 
         mMapFragView = inflater.inflate(R.layout.fragment_map, container, false);
 
+        //TODO: stop it from complaining about API 14 http://stackoverflow.com/questions/26592889/mapfragment-or-mapview-getmap-returns-null-on-lollipop#answer-27681586
+        MapFragment mapFragment = (MapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map_view_container);
+
+        if(mapFragment == null){
+            Log.e(TAG, "Could not get Fragment");
+            //TODO: some sort of error code in the UI
+        }
+        else{
+            mapFragment.getMapAsync(this);
+        }
 
         return mMapFragView;
     }
 
     @Override
     public void onDestroyView() {
+        //TODO: stop it from complaining about API 14 http://stackoverflow.com/questions/26592889/mapfragment-or-mapview-getmap-returns-null-on-lollipop#answer-27681586
+        FragmentManager fm = getChildFragmentManager();
+        Fragment mapFragment = fm.findFragmentById(R.id.map_view_container);
+        if (mapFragment != null) {
+            fm.beginTransaction().remove(mapFragment).commit();
+        }
         super.onDestroyView();
-
     }
 
+    @Override
+    public void onMapReady(GoogleMap map){
+        Log.d(TAG, "Map Ready");
+    }
 
 
     @Override
