78039845934031f1c1ab592dbe8519c1027a2c52
Omkar Moghe <omoghe@umich.edu>
2016-02-19 00:31:31 -0500

    coloring updates for events detail fragment

--- app/src/main/java/com/mhacks/android/ui/MainActivity.java
+++ app/src/main/java/com/mhacks/android/ui/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.webkit.WebView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
