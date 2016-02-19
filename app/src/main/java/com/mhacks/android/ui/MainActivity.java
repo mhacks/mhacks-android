7b924b360aa0703910a98ba0327fe327f8274f0f
Ankit Shah <ank.h.shah@gmail.com>
2016-02-19 00:32:18 -0500

    Markers cleared on switch and zoom changed to incldue rackham

--- app/src/main/java/com/mhacks/android/ui/MainActivity.java
+++ app/src/main/java/com/mhacks/android/ui/MainActivity.java
@@ -83,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         // Add the toolbar
