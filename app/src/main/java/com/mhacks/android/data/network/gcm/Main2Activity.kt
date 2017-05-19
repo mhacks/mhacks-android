f06ca4c8eedae98c9323fd3fe66ff7ec0718917b
Jeffrey Chang <changjef@umich.edu>
2017-05-19 01:56:23 -0400

    Updated dependencies

---
+++ app/src/main/java/com/mhacks/android/data/network/gcm/Main2Activity.kt
@@ -0,0 +1,14 @@
+package com.mhacks.android.data.network.gcm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import org.mhacks.android.R
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
