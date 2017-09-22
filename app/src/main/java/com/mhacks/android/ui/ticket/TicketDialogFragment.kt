16d863273a4ea8dcfd3009a159b203a0fbf32b86
Jeffrey Chang <changjef@umich.edu>
2017-09-21 23:11:33 -0400

    Bug fixes

--- app/src/main/java/com/mhacks/android/ui/ticket/TicketDialogFragment.kt
+++ app/src/main/java/com/mhacks/android/ui/ticket/TicketDialogFragment.kt
@@ -52,7 +52,6 @@ class TicketDialogFragment : DialogFragment() {
                     ticket_full_name_text_view.text = user.fullName
                     if (user.university.isEmpty())
                         ticket_school_text_view.text = getString(R.string.no_school)
-
                 },
                 { Timber.d("About to go to Login")
             callback.startLoginActivity() } )
