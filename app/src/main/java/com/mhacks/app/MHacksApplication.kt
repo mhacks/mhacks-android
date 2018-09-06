630b4f8a98b69734d3b334a4c53afb84fa9a24eb
Shashank Murching <shashankmurching@hotmail.com>
2018-09-06 19:32:50 -0400

    attempting to fix login issues

--- app/src/main/java/com/mhacks/app/MHacksApplication.kt
+++ app/src/main/java/com/mhacks/app/MHacksApplication.kt
@@ -33,7 +33,7 @@ class MHacksApplication : DaggerApplication() {
                     MHACKS_GROUP, NotificationManager.IMPORTANCE_HIGH)
 
             notificationChannel.enableLights(true)
-            notificationChannel.lightColor = Color.MAGENTA
+            notificationChannel.lightColor = Color.argb(255, 203, 20, 88)
             notificationChannel.setShowBadge(true)
             notificationChannel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
 
