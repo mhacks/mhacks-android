6d946fe79036c392f8a618e1d0eeda6bd96695bf
Jeffrey Chang <me@jeffchang.io>
2018-09-30 18:09:28 -0700

    Post announcement successful.

--- ui/src/main/java/com/mhacks/app/ui/createannouncement/CreateAnnouncementDialogFragment.kt
+++ ui/src/main/java/com/mhacks/app/ui/createannouncement/CreateAnnouncementDialogFragment.kt
@@ -2,15 +2,18 @@ package com.mhacks.app.ui.createannouncement
 
 import android.arch.lifecycle.Observer
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.mhacks.app.data.models.CreateAnnouncement
 import com.mhacks.app.ui.common.BaseDialogFragment
 import kotlinx.android.synthetic.main.fragment_create_announcements.*
+import org.mhacks.mhacksui.R
 import org.mhacks.mhacksui.databinding.FragmentCreateAnnouncementsBinding
 import timber.log.Timber
 import javax.inject.Inject
@@ -111,11 +114,28 @@ class CreateAnnouncementDialogFragment: BaseDialogFragment() {
 
     private fun subscribeUi(createAnnouncementViewModel: CreateAnnouncementViewModel) {
         createAnnouncementViewModel.createAnnouncement.observe(this, Observer {
-            it?.let { createAnnouncement ->
-                Timber.e("asdf")
-                createAnnouncement
+            it?.let { _ ->
+                Toast.makeText(
+                        context,
+                        R.string.create_announcement_success,
+                        Toast.LENGTH_LONG)
+                        .show()
+                dismiss()
             }
         })
+        createAnnouncementViewModel.toastMessage.observe(this, Observer {
+            it?.let { textMessage ->
+                rootView?.let { rootView ->
+                    textMessage.text?.let { text ->
+                        Snackbar.make(rootView, text, Toast.LENGTH_SHORT).show()
+                    }
+                    textMessage.textResId?.let { textRes ->
+                        Snackbar.make(rootView, textRes, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        })
+
     }
 
     private fun validate() {
