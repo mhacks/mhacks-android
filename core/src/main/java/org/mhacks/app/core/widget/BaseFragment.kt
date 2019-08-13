d63c9b3d483d92d19ce64575c42486e51c9fdb8b
Jeffrey Chang <me@jeffchang.io>
2019-08-12 23:10:56 -0700

    Remove duplicated dependencies

--- core/src/main/java/org/mhacks/app/core/widget/BaseFragment.kt
+++ core/src/main/java/org/mhacks/app/core/widget/BaseFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
-import dagger.android.support.DaggerFragment
 import org.mhacks.app.core.R
 
 /**
