ab9cd50b6534ba388af346fd2918c2e6bf5dbe43
Jeffrey Chang <me@jeffchang.io>
2019-10-10 13:06:48 -0700

    Add splash screen

--- app/src/main/java/org/mhacks/app/ui/main/SplashActivity.kt
+++ app/src/main/java/org/mhacks/app/ui/main/SplashActivity.kt
@@ -1,19 +1,37 @@
 package org.mhacks.app.ui.main
 
-import android.graphics.drawable.Animatable
+import android.content.Intent
+import android.graphics.drawable.Animatable2
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import org.mhacks.app.R
+import org.mhacks.app.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
-        val animation = findViewById<ImageView>(R.id.splash).drawable as Animatable
-        animation.start()
+        val binding =
+                DataBindingUtil.setContentView<ActivitySplashBinding>(
+                        this, R.layout.activity_splash
+                )
+
+        (binding.activitySplashAnimationImageView.drawable as? AnimatedVectorDrawable)?.let {
+            it.registerAnimationCallback(object : Animatable2.AnimationCallback() {
+                override fun onAnimationEnd(drawable: Drawable?) {
+                    super.onAnimationEnd(drawable)
+                    startActivity(
+                            Intent(this@SplashActivity, MainActivity::class.java)
+                    )
+                }
+            })
+            it.start()
+        }
+
     }
 
 }
